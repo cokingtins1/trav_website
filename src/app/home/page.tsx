9d0155aa82cbf910ca406b99575e878c57a2ff79
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-12-10 17:08:29 -0500

    working on layout

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,25 +5,56 @@ import logoImg from '/public/Logo.png'
 
 export default function Home() {
     return (
+        // <main className={styles.main} >
+        //     <div className={styles.header}>
+        //         <Image src={logoImg}
+        //             className={styles.logoImg}
+        //             width={200}
+        //             alt='logo'></Image>
+        //         <h1>Trav</h1>
+        //     </div>
+        //     <div className={styles.heroSection}>
+        //         <div className={styles.heroRing}></div>
+        //         <Image src={heroImg}
+        //             className={styles.heroImg}
+        //             width={600}
+        //             style={{
+        //                 maxWidth: '100%',
+        //                 height: 'auto'
+        //             }}
+        //             alt='pic'>
+
+        //         </Image>
+
+        //     </div>
+        // </main>
+
         <main className={styles.main} >
             <div className={styles.header}>
                 <Image src={logoImg}
                     className={styles.logoImg}
                     width={200}
                     alt='logo'></Image>
-
+                <h1>Trav</h1>
             </div>
             <div className={styles.heroSection}>
-                <Image src={heroImg}
-                    className={styles.heroImg}
-                    width={500}
-                    height={500}
-                    style={{
-                        maxWidth: '100%',
-                        height: 'auto'
-                    }}
-                    alt='pic'></Image>
+                <div className={styles.heroImgContainer}>
+                    <div className={styles.heroRing}>
+                        <Image src={heroImg}
+                            className={styles.heroImg}
+                            width={600}
+                            style={{
+                                maxWidth: '100%',
+                                height: 'auto'
+                            }}
+                            alt='pic'>
+                        </Image>
+                    </div>
+                </div>
+
             </div>
         </main>
+
     )
 }
+
